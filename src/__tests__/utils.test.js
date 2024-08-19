0a0d07a24abb26f483800a06278fdab6463d5d33
Bridget <bridget.njoki@student.moringaschool.com>
2024-08-19 19:15:36 +0300

    add

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
 // Your tests here
+const isPalindrome = require('../utils');
+
+describe('isPalindrome', () => {
+  test('returns true for a palindrome word', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  test('returns false for a non-palindrome word', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  test('returns true for a case-insensitive palindrome', () => {
+    expect(isPalindrome('RaceCar')).toBe(true);
+  });
+
+  test('returns false for an empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+
+  test('throws an error for non-alphabetic characters', () => {
+    expect(() => isPalindrome('racecar!')).toThrow('Input must contain only alphabetic characters');
+  });
+
+  test('throws an error if input is not a string', () => {
+    expect(() => isPalindrome(123)).toThrow('Input must be a string');
+  });
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,24 @@
 // Your code here
+// src/utils.js
+
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+    
+    if (word === '') {
+      return false; // Consider empty string as not a palindrome
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error('Input must contain only alphabetic characters');
+    }
+  
+    const cleanedWord = word.toLowerCase();
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    return cleanedWord === reversedWord;
+  }
+  
+  module.exports = isPalindrome;
+  
+  
